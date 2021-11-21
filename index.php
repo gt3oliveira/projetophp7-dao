aa9989973523bca35931e19ddda315a3d866fa0b
gt3oliveira <gt3oliveira@gmail.com>
2021-11-21 16:06:10 -0300

    MODIFICAÇÕES NA CLASS USUARIO - ATUALIZEI O FORMAT DE DATA.

--- index.php
+++ index.php
@@ -2,10 +2,18 @@
 
 require_once("config.php");
 
+$root = new Usuario();
+
+$root->loadById(6);
+
+echo $root;
+
+/*
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * from tb_usuarios order by idusuario");
 
 echo json_encode($usuarios);
+*/
 
 ?>
